368fe52b85f711fda2e7f915f3c3b45ea0cdfde5
zwezaynyithegod23 <zwezaynyi@gmail.com>
2025-05-17 18:34:18 +0700

    renaming the modals

--- components/actions.tsx
+++ components/actions.tsx
@@ -3,6 +3,7 @@
 import { toast } from "sonner";
 import { Link2, Trash2 } from "lucide-react";
 import type { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { ConfirmModal } from "./confirm-modal";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,6 +12,7 @@ import {
 } from "./ui/dropdown-menu";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
+import { Button } from "./ui/button";
 
 interface ActionsProp {
   children: React.ReactNode;
@@ -38,7 +40,7 @@ export function Actions({
 
   const onDelete = () => {
     mutate({ id })
-      .then(() => toast.success("Board deleted!"))
+      .then(() => toast.success("Board deleted"))
       .catch(() => toast.error("Failed to delete board"));
   };
 
@@ -56,11 +58,17 @@ export function Actions({
           <Link2 className="h-4 w-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
-
-        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onDelete}>
-          <Trash2 className="h-4 w-4 mr-2" />
-          Delete
-        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete board"
+          description="Are you sure you want to delete this board? This action cannot be undone."
+          onConfirm={onDelete}>
+          <Button
+            variant="ghost"
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal">
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   );
